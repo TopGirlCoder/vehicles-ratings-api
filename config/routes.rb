3112582426cad481f5d1ee21c72ae77a462e96e2
Tanya Lindermeier <topgirlcoder@gmail.com>
2017-03-21 12:32:44 -0700

    adds override to constraints for the api in routes to prepare for deployment on Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-	namespace :api, defaults: { format: :json } do
+	namespace :api, defaults: { format: :json }, path: '/api' do
 	  resources :cars do
 	  	resources :ratings
 		end
